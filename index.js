5fe855c942457b226a2f7532f1ccf20041c349ba
jinook929 <jinook929@gmail.com>
2021-04-12 21:05:39 -0400

    commit tree 1d5b97a371354c467b9af4e84ccaee7bd49d4259

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(res => res.json())
+  .then(obj=> {
+    console.log(obj);
+    const body = document.querySelector("body");
+    body.textContent = obj.id;
+  })
+  .catch(error => {
+    console.log(error.message);
+    const body = document.querySelector("body");
+    body.textContent = error.message;
+  });
+}
+
+submitData("John", "john@email.com");
